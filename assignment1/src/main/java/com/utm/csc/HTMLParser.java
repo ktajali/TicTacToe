93aa8edc4017bf8dfd9e9b57632d778c3dac29ec
ktajali <k.tajalizadekhoob@mail.utoronto.ca>
2016-01-24 12:50:37 -0500

    submitting the first assignment

--- assignment1/src/main/java/com/utm/csc/HTMLParser.java
+++ assignment1/src/main/java/com/utm/csc/HTMLParser.java
@@ -1,7 +1,42 @@
 package com.utm.csc;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class HTMLParser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		//getting the url from user
+		Scanner scanner = new Scanner(System.in);
+		
+		//getting results of all urls through while loop
+		while(scanner.hasNext()){
+			
+			String url = scanner.next();
+			//creating a Jsoup doc object to process url
+			Document doc = Jsoup.connect(url).get();
+		
+			System.out.println("title : " + doc.title());
+			
+			Elements links = doc.select("a[href]");
+			
+		    //processing links in the url
+			for(Element link : links)
+			{
+				System.out.println("");
+				
+				System.out.println("link : " + link.attr("href"));
+				
+				System.out.println("text : " + link.text());
+			}
+		}
+		
+		
 		
 	}
 }
