b99e1ea0f32fd9ce58f318333e2355852589fafd
ktajali <k.tajalizadekhoob@mail.utoronto.ca>
2016-03-09 19:08:49 -0500

    add class TicTacToe to handle the player and the game state

--- assignment3/src/main/java/com/utm/csc/App.java
+++ assignment3/src/main/java/com/utm/csc/App.java
@@ -8,55 +8,40 @@ public class App
     {
       //scanner to scan the user inputs
       Scanner scan = new Scanner(System.in);
-      //game board
-      Board board = new Board();
-      //initializing the game board-set the cells to empty char
-      board.init();
-      //put main player as O 
-      char mainPlayer = 'X';
-      int row = 0;
-      int col = 0;
-      //if the player played his round successfully, change becomes true to change the player
+
+      TicTacToe game = new TicTacToe('X');
+      
+      //change is true if the player moved successfully
       boolean change = false;
       System.out.println("Enter '<row>,<col>' to play a position. For example, '0,2'.");
+      
       //x and y holding the row and col temporarily
       int x = 0 , y = 0;
 
-      while (!board.won(row , col, mainPlayer) && !board.full()){
-        //switch the player every time if the last player inserted successfully
-        if (change){
-          mainPlayer = (mainPlayer == 'X') ? 'O' : 'X';
-        }
-      
-        System.out.print("Player " + mainPlayer + " [row,col]: ");
+      while (!game.win() && !game.tie()){
+        
+        System.out.print("Player " + game.getPlayer() + " [row,col]: ");
         //get the string user inputs-handles if user enter a wrong format
         try{
           String [] line = scan.nextLine().split(",");
           x = Integer.parseInt(line[0]);
           y = Integer.parseInt(line[1]);
-          change = board.setCell(x,y, mainPlayer);
+          change = game.play(x, y);
+        
         }catch (Exception ex)
         {
-          change = false;
           System.err.println("Wrong input format! Enter '<row>,<col>' to play a position. For example, '0,2'.");
         }
-        //set the cell in the game board
-        
+
+          //printing the game board*/
         if(change)
-        {
-          row = x;
-          col = y;
-          //printing the game board
-          System.out.println(board);
-        }
+          System.out.println(game.getBoard());
         else
           System.out.println("");
-
-
-        
+  
       }
-      if(board.won(row , col, mainPlayer))
-        System.out.println("Player " + mainPlayer + " won");
+      if(game.win())
+        System.out.println("Player " + game.getPlayer() + " won");
       else
         System.out.println("Nobody won the game :(");
       
