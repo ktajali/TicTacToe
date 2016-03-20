756377947fdc3f9f52e2a754c357b07277841203
Abbas A <abbas.attarwala@utoronto.ca>
2016-03-20 10:32:38 -0400

    Adding Assignment4 starter code

---
+++ assignment4/src/main/java/com/utm/csc/App.java
@@ -0,0 +1,13 @@
+package com.utm.csc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Assignment3" );
+    }
+}
